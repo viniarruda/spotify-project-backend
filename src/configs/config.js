6429df995fcb76055e8a35cfe5b5c21cc1a6eb86
Vinicius Arruda <viniarruda.souza@gmail.com>
2019-05-26 01:58:50 +0000

    Update server

--- src/configs/config.js
+++
@@ -1,8 +0,0 @@
-const configs = {
-  client_id: '5241c782aa824632850c999b3047a904',
-  response_type: 'code',
-  redirect_uri: 'http://localhost:8888/callback',
-  scope: 'user-read-private&user-read-email'
-}
-
-module.exports = configs;
